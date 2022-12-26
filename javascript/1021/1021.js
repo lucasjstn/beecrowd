b2d04dffe3183b2bd23fcd3e2b54dcd899b609ec
lucasjstn <lucas7justino@gmail.com>
2022-12-26 14:25:13 -0300

    1021 solved but not sending in the beecrowd platform

--- javascript/1021/1021.js
+++ javascript/1021/1021.js
@@ -1,24 +1,45 @@
 // var input = require("fs").readFileSync("/dev/stdin", "utf8");
-var input = "576.73";
+var input = "4";
 var lines = input.split("\n");
 
 let entry = Number(lines[0]);
-let notas = [100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.5, 0.01];
+let notas = [
+  100.0,
+  50.0,
+  20.0,
+  10.0,
+  5,
+  2,
+  1,
+  "0.50",
+  0.25,
+  "0.10",
+  0.05,
+  0.01,
+];
 let manyNote = [];
 // console.log(entry);
 
 for (let i = 0; i < notas.length; i++) {
   if (entry > notas[i]) {
     manyNote[i] = manyNotes(entry, notas[i]);
-    entry = entry % notas[i];
-  }
 
-  //   if()
-  console.log(entry);
+    entry = entry % notas[i];
+  } else {
+    manyNote[i] = 0;
+  }
+  if (i === 0) {
+    console.log(`NOTAS:`);
+  } else if (i == 6) {
+    console.log(`MOEDAS:`);
+  }
+  console.log(
+    `${manyNote[i]} ${i < 6 ? "nota" : "moeda"}(s) de R$ ${notas[i]}${
+      i < 7 ? ".00" : ""
+    }`
+  );
 }
 
-console.log(manyNote);
-
 function manyNotes(num, note) {
-  return Math.floor(num / note);
+  return Math.floor(num.toFixed(2) / Number(note).toFixed(2));
 }
