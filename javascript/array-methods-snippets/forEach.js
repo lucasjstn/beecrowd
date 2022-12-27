fca89483062785041ab87c3d55d2ef4496d96f52
lucasjstn <lucas7justino@gmail.com>
2022-12-27 05:28:13 -0300

    foreach

---
+++ javascript/array-methods-snippets/forEach.js
@@ -0,0 +1,6 @@
+// forEach(): executes a provided function once for each array element.
+let array = [1, 2, 3, 4, 5];
+
+const a = array.forEach(function (element) {
+  console.log(element * 2);
+});
