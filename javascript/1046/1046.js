80e11c2b2efabc5df150eb7309e6649b15df89f1
lucasjstn <lucas7justino@gmail.com>
2023-01-25 22:26:07 -0300

    learning some rust code

--- javascript/1046/1046.js
+++ javascript/1046/1046.js
@@ -1,19 +1,13 @@
 var input = require("fs").readFileSync("stdin", "utf8");
 var lines = input.split(" ");
 
-const entries = lines.map((element) => {
-  return Number(element);
-});
 
-const [initialHour, finalHour] = entries;
+const [initialHour, finalHour] = [0, 0];
 
-console.log(16 - 24);
-// console.log(initialHour, finalHour);
+console.log(`O JOGO DUROU ${24 - initialHour + finalHour} HORA(S)`);
 // Hora inicial 0 - 16 = -8
 // Hora final   2 - 0   = 2
 
-// Hora inicial 0 - 0 = 0
 // Hora final   24 - 0 = 24
 
 // Hora inicial 0 - 2 = -2
-// Hora final   16 - 0 = 16
---
+++ rust/src/1000/main.rs
@@ -0,0 +1,7 @@
+#![allow(unused)]
+
+// comments
+
+fn main() {
+    println!("Hello World!\n\n");
+}
