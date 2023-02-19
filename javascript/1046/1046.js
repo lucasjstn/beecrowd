7c4329e4dc3eb32ef05822929ba11f24cbb8fe92
lucasjstn <lucas7justino@gmail.com>
2023-02-19 12:58:32 -0300

    1046 solved

--- javascript/1046/1046.js
+++ javascript/1046/1046.js
@@ -1,13 +1,13 @@
 var input = require("fs").readFileSync("stdin", "utf8");
 var lines = input.split(" ");
 
+let start = Number(lines[0])
+let end = Number(lines[1]);
 
-const [initialHour, finalHour] = [0, 0];
-
-console.log(`O JOGO DUROU ${24 - initialHour + finalHour} HORA(S)`);
-// Hora inicial 0 - 16 = -8
-// Hora final   2 - 0   = 2
-
-// Hora final   24 - 0 = 24
-
-// Hora inicial 0 - 2 = -2
+if(start > end) {
+	console.log("O JOGO DUROU " + (24 - start + end) +" HORA(S)")
+} else if(end > start) {
+	console.log("O JOGO DUROU " + (end - start) + " HORA(S)")
+} else {
+	console.log("O JOGO DUROU "+ 24 + " HORA(S)")
+}
