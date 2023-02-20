ed26deb8804cce82b1151f9f22f5b8862ada0955
lucasjstn <lucas7justino@gmail.com>
2023-02-20 10:52:00 -0300

    singly linked list

---
+++ c/singly-linked-list.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	 struct abc {
+		 int a;
+		 char b;
+		 struct abc *self;
+	 }
+
+	 printf(abc);
+}
---
+++ c/teste.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	int num, original, sum, rem = 0;
+	printf("Enter A number");
+}
+
